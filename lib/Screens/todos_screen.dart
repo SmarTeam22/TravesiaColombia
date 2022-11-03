9e4b2000f46d2194a4da36d2e7b5c46e44c3584c
SanDev305 <santiagomedina.123@gmail.com>
2022-11-02 21:46:58 -0500

    Se añade el home

--- lib/Screens/todos_screen.dart
+++ lib/Screens/todos_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travesia_colombia2022/model/todos_model.dart';
+import 'package:travesia_colombia2022/pages/turistico_poi.dart';
 
 class TodosScreen extends StatefulWidget {
   const TodosScreen({Key? key}) : super(key: key);
@@ -10,10 +12,37 @@ class TodosScreen extends StatefulWidget {
 class _TodosScreenState extends State<TodosScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text("Todos"),
-      ),
+
+    return ListView.builder(
+
+      itemCount: temporalData.length,
+      itemBuilder: (context,i) => Column(
+        children: <Widget> [
+          TextButton(
+            style: ButtonStyle(),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TuristicoPOIPage()));
+              },
+              child: Column(
+                children: [
+                  ListTile(leading: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(temporalData[i].img,)),
+                      title: Text(temporalData[i].lugar, style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                      subtitle: Container(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Text(temporalData[i].descripcion, style: TextStyle(color: Colors.grey, fontSize: 15.0),overflow: TextOverflow.ellipsis),
+                      )
+                  ),
+
+                ],
+              )),
+          const Divider(height: 1,),
+        ],
+      )
     );
   }
 }
---
+++ lib/model/todos_model.dart
@@ -0,0 +1,40 @@
+class TodosClass{
+
+  final String lugar;
+  final String descripcion;
+  final String img;
+
+  TodosClass(this.lugar, this.descripcion, this.img);
+}
+  List<TodosClass> temporalData = [
+
+   TodosClass(
+       "lugar1",
+       "descripcion111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111",
+       "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+    TodosClass(
+        "lugar1",
+        "descripcion1",
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"),
+
+  ];
